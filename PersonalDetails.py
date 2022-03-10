fc4a3c434b68169142270ea1f0b46c43261c8e0a
Aadharv <101254088+Aadharv@users.noreply.github.com>
2022-03-10 15:34:37 +0530

    Add files via upload

---
+++ PersonalDetails.py
@@ -0,0 +1,7 @@
+name= "Aadharv"
+school= "sishya"
+country= "Chennai"
+age= 11
+DOB= "5/7/2010"
+
+print("Hello my name is",name ,".I live in",country,".I study at",school,".I was born on",DOB,".I am",age,"years old.")
